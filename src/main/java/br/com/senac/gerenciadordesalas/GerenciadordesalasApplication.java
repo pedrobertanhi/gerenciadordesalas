74d7539c32d44f4bc8073081f277c233827a0f45
Enzo Alvesi <enzoaugustorodrigues24@gmail.com>
2026-08-30 14:27:23 -0300

    feat: adiciona configuracao do flyway e migration inicial v1

---
+++ src/main/java/br/com/senac/gerenciadordesalas/GerenciadordesalasApplication.java
@@ -0,0 +1,13 @@
+package br.com.senac.gerenciadordesalas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadordesalasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadordesalasApplication.class, args);
+	}
+
+}
